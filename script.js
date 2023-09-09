32bc92e051c23a2c64fe7280c622c332fc6794f7
EGRrqq <86194387+EGRrqq@users.noreply.github.com>
2023-09-09 18:36:57 +0500

    chore(js): add validation for password input
    - At least 8 characters
    - At least 1 lowercase character [a-z]
    - At least 1 uppercase character [A-Z]
    - At least 1 numeric character [0-9]
    - At least 1 special character [@$!%*?&]
    
    These messages appear only when the user
    - clicks on an input field
    - focuses on the input field
    
    chore(html): comment out all input except password input
    chore(css): add gap and margin for ul
    chore(css): add spaces for unicode characters

--- script.js
+++ script.js
@@ -8,32 +8,111 @@
 const form = document.querySelector("form");
 const password = document.getElementById("password");
 
-const passwordContainer = document.querySelector(".password-container");
-const errorList = document.createElement("ul");
-const errorItem = document.createElement("li");
+const pattern = {
+  minLength: {
+    validate: false,
+    message: "At least 8 characters",
+    link: '',
 
-errorList.appendChild(errorItem);
-passwordContainer.appendChild(errorList);
+    test: function (str) {
+      const regex = new RegExp(".{8,}");
+
+      regex.test(str) ? (this.validate = true) : (this.validate = false);
+    },
+  },
+  lowerCase: {
+    validate: false,
+    message: "At least 1 lowercase character [a-z]",
+    link: '',
+
+    test: function (str) {
+      const regex = new RegExp(".*?[a-z]");
+
+      regex.test(str) ? (this.validate = true) : (this.validate = false);
+    },
+  },
+  upperCase: {
+    validate: false,
+    message: "At least 1 uppercase character [A-Z]",
+    link: '',
+
+    test: function (str) {
+      const regex = new RegExp(".*?[A-Z]");
+
+      regex.test(str) ? (this.validate = true) : (this.validate = false);
+    },
+  },
+  number: {
+    validate: false,
+    message: "At least 1 numeric character [0-9]",
+    link: '',
+
+    test: function (str) {
+      const regex = new RegExp(".*?[0-9]");
+
+      regex.test(str) ? (this.validate = true) : (this.validate = false);
+    },
+  },
+  specialCharacter: {
+    validate: false,
+    message: "At least 1 special character [@$!%*?&]",
+    link: '',
+
+    test: function (str) {
+      const regex = new RegExp(".*?[@$!%*?&]");
+
+      regex.test(str) ? (this.validate = true) : (this.validate = false);
+    },
+  },
+};
+
+let listCreated = false;
+
+function requirementsList() {
+  if (!listCreated) {
+    const passwordContainer = document.querySelector(".password-container");
+    const errorList = document.createElement("ul");
+
+    for (let item in pattern) {
+      const errorItem = document.createElement("li");
+      pattern[item].link = errorItem;
+      errorItem.classList = "form-error";
+      errorItem.textContent = pattern[item].message;
+      errorList.appendChild(errorItem);
+    }
+
+    passwordContainer.appendChild(errorList);
+    listCreated = true;
+
+    ["click", "focusin"].forEach((e) =>
+      window.addEventListener(e, (event) => {
+        if (!passwordContainer.contains(event.target)) {
+          errorList.remove();
+          listCreated = false;
+        }
+      })
+    );
+  }
+}
+
+password.addEventListener("click", requirementsList);
+password.addEventListener("focusin", requirementsList);
 
 password.addEventListener("input", (event) => {
-  if (password.validity.valid) {
-    errorItem.classList = "form-success";
-  } else {
-    showError();
+  for (let item in pattern) {
+    console.log(event.target.value);
+    pattern[item].test(event.target.value);
+
+    pattern[item].validate
+      ? (pattern[item].link.classList = "form-success")
+      : (pattern[item].link.classList = "form-error");
   }
 });
 
 form.addEventListener("submit", (event) => {
-  if (!password.validity.valid) {
-    showError();
+  if (!password.validity.patternMismatch && !password.validity.valueMissing) {
+    alert("deez nuts");
   } else {
     event.preventDefault();
   }
 });
-
-function showError() {
-  if (password.validity.tooShort) {
-    errorItem.classList = 'form-error'
-    errorItem.textContent = ` Password should be at least ${password.minLength} characters`;
-  }
-}
